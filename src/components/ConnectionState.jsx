e2ddc01efe986b1433cad409485a1473f9cf1f61
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2025-02-28 20:54:18 -0800

    Empty to follow set up in api format

--- src/components/ConnectionState.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from "prop-types";
-
-export function ConnectionState({ isConnected }) {
-  return <p>State: { '' + isConnected }</p>;
-}
-
-ConnectionState.propTypes = {
-  isConnected : PropTypes.bool
-}
