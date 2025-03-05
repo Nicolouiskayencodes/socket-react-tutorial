1fc80c0534b9a484fa278f0d095a179ee928c018
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2025-03-04 21:46:25 -0800

    Add event to commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { socket } from './socket.js';
 import { ConnectionManager } from './components/connectionManage.jsx';
 import './App.css'
 
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
+  const input = useRef(null)
   useEffect(() => {
     function onConnect() {
       setIsConnected(true);
@@ -24,12 +25,20 @@ function App() {
     };
   }, []);
 
+  function sendMessage(event) {
+    event.preventDefault();
+    if (input.current.value) {
+      socket.emit('chat message', input.current.value);
+      input.current.value = '';
+    }
+  }
+
   return (
     <>
       <ConnectionManager />
       <ul id="messages"></ul>
       <form id="form" action="">
-        <input id="input" autocomplete="off" /><button>Send</button>
+        <input id="input" autocomplete="off" ref={input}/><button onClick={sendMessage}>Send</button>
       </form>
     </>
   )
